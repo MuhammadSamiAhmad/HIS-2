3dec4dbf6256ce213468e2157ad8898543198df5
MuhammadSamiAhmad <mrmidosami@yahoo.com>
2024-11-25 18:48:29 +0200

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -21,15 +20,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p className="font-manrope text-Silver-1">Edit and save to test HMR</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#FEFEFE",
+        textColor: "#253446",
+        callToAction: "#3D5CEF",
+        Silver: {
+          1: "#7E899E",
+          2: "#ACB3C3",
+          3: "#E0E1E6",
+          4: "#E9EAEC",
+          5: "#F3F4F6",
+          6: "#F0F2FE",
+        },
+        UtilColors: {
+          Blue: "#63ADFF",
+          Pink: "#E8588A",
+          Green: "#46B988",
+          Orange: "#FDB335",
+          Red: "#E95151",
+        },
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
